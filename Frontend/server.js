d1958d1b9fcd9101c94f31722223a7be21f5ce8f
ucx15 <83822686+ucx15@users.noreply.github.com>
2024-12-03 09:44:06 +0530

    Made it a Node.Js project seperated Backend and Frontend

---
+++ Frontend/server.js
@@ -0,0 +1,13 @@
+// express frontend server
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
