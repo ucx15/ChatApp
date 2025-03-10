82eab4fff402c2d2e34fb49772b71743bf38d74a
ucx15 <83822686+ucx15@users.noreply.github.com>
2025-03-10 19:39:57 +0530

    Bug Fix: using http on local network

--- Frontend/Public/Scripts/global.js
+++ Frontend/Public/Scripts/global.js
@@ -2,12 +2,17 @@
 const BACKEND_HOST = window.location.hostname;
 
 let BACKEND_PORT = 80;
-if (BACKEND_HOST === "localhost") {
+let PROTOCOL1 = "https";
+let PROTOCOL2 = "wss";
+
+if (BACKEND_HOST === "localhost" || BACKEND_HOST.startsWith("192.168.")) {
 	BACKEND_PORT = 5000;
+	PROTOCOL1 = "http";
+	PROTOCOL2 = "ws";
 }
 
-const BACKEND_URI = `http://${BACKEND_HOST}:${BACKEND_PORT}`;
-const WS_BACKEND_URI = `ws://${BACKEND_HOST}:${BACKEND_PORT}/ws`;
+const BACKEND_URI    = `${PROTOCOL1}://${BACKEND_HOST}:${BACKEND_PORT}`;
+const WS_BACKEND_URI = `${PROTOCOL2}://${BACKEND_HOST}:${BACKEND_PORT}/ws`;
 
 const RECONNECT_DELAY = 5000; // ms
 const MAX_RECONNECT_ATTEMPTS = 10;
